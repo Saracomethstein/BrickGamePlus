25eadc454c5e54383c97e3667cf5d99e6d22cc16
Adel Sevastianov <102406080+Saracomethstein@users.noreply.github.com>
2024-09-18 18:11:52 +0300

    up: extern tetris

--- brick_game/tetris/model/src/extern_tetris.cc
+++ brick_game/tetris/model/src/extern_tetris.cc
@@ -13,7 +13,46 @@ void TetrisGame::HandleKey(Keys k) {
     case Keys::Key_Left:
       action = Left;
       break;
+    case Keys::Key_Right:
+      action = Right;
+      break;
+
+    case Keys::Key_Up:
+      action = Up;
+      break;
+
+    case Keys::Key_Down:
+      action = Down;
+      break;
+
+    case Keys::Key_Space:
+      action = Rotation;
+      break;
+
+    case Keys::Key_ESC:
+      action = Terminate;
+      break;
+  }
+  user_input(tetris_game_, action);
+}
+
+void TetrisGame::GetData(GameInfo &gi) const {
+  gi.field = tetris_game_->field;
+  gi.next = tetris_game_->next;
+  gi.score = tetris_game_->score;
+  gi.record = tetris_game_->high_score;
+  gi.level = tetris_game_->level;
+  gi.speed = tetris_game_->speed;
+  gi.pause = tetris_game_->pause;
+  gi.block_row = tetris_game_->block_row;
+  gi.block_col = tetris_game_->block_col;
+  for (int i = 0; i < BLOCK_SIZE; ++i) {
+    for (int j = 0; j < BLOCK_SIZE; ++j) {
+      gi.block[i][j] = tetris_game_->block[i][j];
+    }
   }
 }
+
+void TetrisGame::MakeTick() { move_down(tetris_game_); }
 
 };  // namespace s21
