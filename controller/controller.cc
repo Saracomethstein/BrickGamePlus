79954cec3c16ba37cc162939c7445cbd23753957
Adel Sevastianov <102406080+Saracomethstein@users.noreply.github.com>
2024-09-23 14:02:25 +0300

    up: controller class

--- controller/controller.cc
+++ controller/controller.cc
@@ -1,1 +1,43 @@
 #include "controller.h"
+
+GameController::GameController() : currentGame(nullptr) {}
+
+void GameController::SetGame(GameName gn) {
+  game = gn;
+  switch (game) {
+    case GameName::TETRIS:
+      currentGame = std::make_unique<s21::TetrisGame>();
+      break;
+
+      // Edit snake class for arcade game //
+      // case GameName::SNAKE:
+      //   currentGame = std::make_unique<s21::Snake>();
+      //   break;
+
+    case GameName::NONE:
+      currentGame.reset();
+      currentGame = nullptr;
+      break;
+  }
+}
+
+void GameController::HandleKey(ArcadeGame::Keys key) {
+  if (currentGame == nullptr) {
+    return;
+  }
+  currentGame->HandleKey(key);
+}
+
+void GameController::MakeTick() {
+  if (currentGame == nullptr) {
+    return;
+  }
+  currentGame->MakeTick();
+}
+
+void GameController::GetData(GameInfo &gi) {
+  MakeTick();
+  if (currentGame != nullptr) {
+    currentGame->GetData(gi);
+  }
+}
--- controller/controller.h
+++ controller/controller.h
@@ -1,13 +1,20 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-namespace s21 {
+#include "../brick_game/common/common_class.h"
+#include "../brick_game/common/common_types.h"
+#include "../brick_game/snake/model/include/model.h"
+#include "../brick_game/tetris/model/include/extern_model.h"
+
 class GameController {
  public:
   GameController();
-  void InitGame();
-  void UpdateGame();
-  void RenderGame();
+  void SetGame(GameName gn);
+  void HandleKey(ArcadeGame::Keys key);
+  void MakeTick();
+  void GetData(GameInfo &gi);
+
+  GameName game;
+  std::unique_ptr<ArcadeGame> currentGame;
 };
-}  // namespace s21
 #endif
